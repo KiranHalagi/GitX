8fa68bc429251178d2892e35c686804a9ab22f8b
kiran <kiranhalagi7@gmail.com>
2020-11-11 12:14:56 +0530

    kiran second commit in Api Automation

--- src/test/java/apiScripts/Addplace.java
+++ src/test/java/apiScripts/Addplace.java
@@ -10,6 +10,9 @@ public class Addplace {
 	public static void AddPlace()
 	{
 		System.out.println("place added successfullly");
+		
+		System.out.println("hi hello kiran");
+		System.out.println("hi hello harish");
 	}
 
 }
